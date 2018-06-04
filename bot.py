ec53b7698fd959f5577b5512033e2c1e74b7d094
10001shh <sghsri@gmail.com>
2018-06-04 10:30:57 -0500

    uses subreddit filters

--- bot.py
+++ bot.py
@@ -14,10 +14,14 @@ else:
        posts_replied_to = f.read()
        posts_replied_to = posts_replied_to.split("\n")
        posts_replied_to = list(filter(None, posts_replied_to))
-def reddit(sub):
+def reddit(sub, fil):
 	reddit = praw.Reddit('bot1')
 	subreddit = reddit.subreddit(sub)
-	for submission in subreddit.top(limit=10):
+	if fil == "top":
+		list = subreddit.top(limit=10)
+	elif fil == "hot":
+		list = subreddit.hot(limit=10)
+	for submission in list:
 		posts_replied_to.append(submission.title+"\t"+str(submission.score))
 			# if re.search("i love python", submission.title, re.IGNORECASE):
 			# 	submission.reply("Botty bot says: Me too!!")
@@ -26,6 +30,6 @@ def reddit(sub):
 		for post_id in posts_replied_to:
 			f.write(post_id + "\n")
 def main():
-  reddit(sys.argv[1])
+  reddit(sys.argv[1], sys.argv[2])
 if __name__ == '__main__':
   main()
