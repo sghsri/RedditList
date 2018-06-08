63f597d32ba50714360c2d4a00dc669277763fa2
10001shh <sghsri@gmail.com>
2018-06-08 10:51:04 -0500

    top posts in subreddit bot

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import sys
 
 
 
-
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
 else:
@@ -19,17 +18,16 @@ def reddit(sub, fil):
 	subreddit = reddit.subreddit(sub)
 	if fil == "top":
 		list = subreddit.top(limit=10)
-	elif fil == "hot":
+	elif fil == "hot": 
 		list = subreddit.hot(limit=10)
 	for submission in list:
-		posts_replied_to.append(submission.title+"\t"+str(submission.score))
-			# if re.search("i love python", submission.title, re.IGNORECASE):
-			# 	submission.reply("Botty bot says: Me too!!")
-			# 	print("Bot replying to : ", submission.title)
+		posts_replied_to.append(submission.title+"-"+str(submission.score)+"\n"+str(submission.url))
 	with open("topposts.txt", "w") as f:
-		for post_id in posts_replied_to:
-			f.write(post_id + "\n")
-def main():
-  reddit(sys.argv[1], sys.argv[2])
+		for post in posts_replied_to:
+			f.write(post + "\n")
+def main():	
+	sub = input("subreddit? ")
+	fil = input("top/hot? ")
+	reddit(sub,fil)
 if __name__ == '__main__':
   main()
